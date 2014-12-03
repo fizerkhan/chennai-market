db12b6a56d27c42a48116652849b033e4e6f754b
Fizer Khan <fizerkhan@gmail.com>
2014-12-04 00:38:42 +0530

    Updated release file

--- www/js/services.js
+++ www/js/services.js
@@ -2,10 +2,10 @@ angular.module('chennaiMarket.services', [])
 .factory('KimonoService', function($http) {
   return {
     getVegetables: function() {
-      return $http.jsonp('https://www.kimonolabs.com/api/6jfd7ua0?apikey=5608c5fa9f5c237d8ffc08188fc716e0&callback=JSON_CALLBACK');
+      return $http.jsonp('https://www.kimonolabs.com/api/71oradvm?apikey=5608c5fa9f5c237d8ffc08188fc716e0&callback=JSON_CALLBACK');
     },
     getFruits: function() {
-      return $http.jsonp('https://www.kimonolabs.com/api/eet4vfik?apikey=5608c5fa9f5c237d8ffc08188fc716e0&callback=JSON_CALLBACK');
+      return $http.jsonp('https://www.kimonolabs.com/api/8lh3e9ki?apikey=5608c5fa9f5c237d8ffc08188fc716e0&callback=JSON_CALLBACK');
     },
     getGoldSilver: function() {
       return $http.jsonp('https://www.kimonolabs.com/api/ds5knnw6?apikey=5608c5fa9f5c237d8ffc08188fc716e0&callback=JSON_CALLBACK');
